2e6400a2b9cdec7f3e000eb0d43bb5e359cef006
whatmakesaman <iphone0727@naver.com>
2025-11-20 01:56:12 +0900

    Add files via upload

---
+++ sql/store_list_r(korea,chinese).sql
@@ -0,0 +1,6 @@
+use fooddb;
+-- example --
+/*
+INSERT INTO store (name, address, open_time, close_time, phone, distance_km, category_id)
+VALUES ('', ' 1', '10:00','22:00','010-1111-2222', 0.8, 1);
+*/
